4d362d50dfadb3eaeca42a6982804afcdeba71f6
Christian Franck <christian.franck@stud.tu-darmstadt.de>
2022-06-21 16:58:38 +0200

    upgrate go-ethereum binding script

--- contracts/generate.sh
+++ contracts/generate.sh
@@ -2,29 +2,49 @@
 
 set -e
 
-if [ -z "$ABIGEN" ]
+# Define ABIGEN and SOLC default values.
+ABIGEN="${ABIGEN-abigen}"
+SOLC="${SOLC-solc}"
+
+echo 'Please ensure that solc v0.7.6+ and abigen v1.10.18+ are installed.'
+
+if ! $ABIGEN --version
 then
-    ABIGEN=abigen
+    echo "'abigen' not found. Please add to PATH or set ABIGEN='path_to_abigen'."
+    exit 1
 fi
 
-if [ -z "$SOLC" ]
+if ! $SOLC --version
 then
-    SOLC=solc
+    echo "'solc' not found. Please add to PATH or set SOLC='path_to_solc'."
+    exit 1
 fi
 
 # Generate golang bindings from solidity contract
 # Argument 1: solidity contract file
 # Argument 2: golang contract name (used for package and file)
 generate_bindings() {
-    CONTRACT_SOL_FILE=$1
-    CONTRACT_GO_NAME=$2
-    PKG=$CONTRACT_GO_NAME
+    FILE=$1; PKG=$2; CONTRACT=$FILE
+    echo "Generating $PKG bindings..."
     GENDIR=./generated/$PKG
-    mkdir -p $GENDIR
-    $ABIGEN --pkg $PKG --sol $CONTRACT_SOL_FILE --out $GENDIR/$CONTRACT_GO_NAME.go --solc $SOLC
-    echo "contract compiled to $GENDIR/$CONTRACT_GO_NAME.go"
+
+    rm -r $GENDIR
+    mkdir $GENDIR
+
+    # Compile and generate binary runtime.
+    $SOLC --abi --bin --bin-runtime --optimize --allow-paths contracts/vendor, $FILE.sol -o $GENDIR/
+    BIN_RUNTIME=$(cat ${GENDIR}/${CONTRACT}.bin-runtime)
+    OUT_FILE="$GENDIR/${CONTRACT}BinRuntime.go"
+    echo "package $PKG" > $OUT_FILE
+    echo >> $OUT_FILE
+    echo "// ${CONTRACT}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts." >> $OUT_FILE
+    echo "var ${CONTRACT}BinRuntime = \"$BIN_RUNTIME\"" >> $OUT_FILE
+
+    # Generate bindings.
+    $ABIGEN --pkg $PKG --abi $GENDIR/$FILE.abi --bin $GENDIR/$FILE.bin --out $GENDIR/$FILE.go &&\
+    echo "contract compiled to $GENDIR/$FILE.go"
 }
 
-generate_bindings ./DominionApp.sol dominionApp
+generate_bindings DominionApp dominionApp
 # generate_bindings ./perun-eth-contracts/contracts/Adjudicator.sol adjudicator
 # generate_bindings ./perun-eth-contracts/contracts/AssetHolderETH.sol assetHolderETH
